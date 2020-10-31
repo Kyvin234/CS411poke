60a31290f0708b8a188d4966fef21b6c998ec5fe
yuey8 <yuey8@illinois.edu>
2020-10-30 21:13:16 -0500

    update bug fixed

--- src/users/views.py
+++ src/users/views.py
@@ -92,6 +92,18 @@ class team_uview(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = team_table
     fields = ['team_name', 'team_comp', 'description']  
     success_url = reverse_lazy('team')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cursor = connection.cursor()
+        cursor.execute('''
+                         SELECT distinct s_name, f_name, dex_id, gen, type1, type2
+                            FROM pokewiki_s_table, pokewiki_f_table
+                            WHERE s_name = s_name_id 
+                            ORDER BY dex_id
+                           ''') 
+        context['pokemon_detail'] = dictfetchall(cursor)
+        return context
+        
     def post(self, request, *args, **kwargs):
         cursor = connection.cursor()
         cursor.execute(

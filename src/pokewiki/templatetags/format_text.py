bb52472a1a9734ab7e48f1bb5aa664a9f1ce3568
yuey8 <yuey8@illinois.edu>
2020-11-28 20:51:24 -0600

    minor fix

--- src/pokewiki/templatetags/format_text.py
+++ src/pokewiki/templatetags/format_text.py
@@ -5,3 +5,27 @@ register = template.Library()
 @register.filter
 def format_text(value):
     return value.replace("-"," ").title()
+
+@register.filter
+def to_roman(num):
+    if num == 1:
+        return 'I'
+    elif num == 2:
+        return 'II'
+    elif num == 3:
+        return 'III'
+    elif num == 4:
+        return 'IV'
+    elif num == 5:
+        return 'V'
+    elif num == 6:
+        return 'VI'
+    elif num == 7:
+        return 'VII'
+    elif num == 8:
+        return 'VIII'
+    elif num == 9:
+        return 'IX'
+    elif num == 10:
+        return 'X'
+    
